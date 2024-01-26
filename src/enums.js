0eba4c6de2ac24a011c069c965016f4fdc40c2d8
khanhthan <khanh.than@sea-solutions.com>
2024-01-26 10:15:47 +0700

    init merchant sdk

---
+++ src/enums.js
@@ -0,0 +1,6 @@
+const BeansMerchantEnvironment = Object.freeze({
+    DEVELOP:   'jop-beans-api.eu.ngrok.io',
+    STAGING:  'api.staging.beansapp.com',
+});
+
+export default BeansMerchantEnvironment;
