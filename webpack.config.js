eec8c47ee4ed6eb1d720daab2fc70632072cf532
Jop Middelkamp <middelkamp@live.nl>
2024-03-21 11:27:59 +0700

    Updated webpack to use ES module

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
-const path = require('path');
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+// Convert __dirname to work with ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // Use environment variable to set mode. Default to 'production' if not set.
 const mode = process.env.NODE_ENV || 'production';
 
-module.exports = {
+export default {
   mode: mode,
   entry: './src/sdk.ts', // your main TypeScript file
   module: {
@@ -22,8 +28,11 @@ module.exports = {
     extensions: ['.ts', '.js'],
   },
   output: {
-    libraryTarget: 'module',
+    module: true, // Use module type for ES module support
     filename: 'sdk.js', // output bundle file name
     path: path.resolve(__dirname, 'dist'), // output path
+    environment: { // Specify the environment features you want to use
+      module: true
+    }
   },
 };
