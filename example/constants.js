8e4a3f9ddc1657f7056984683a607c6938e00fa2
Turan <ahmet.turan.abi@gmail.com>
2024-08-17 18:12:04 -0700

    design update

---
+++ example/constants.js
@@ -0,0 +1,28 @@
+const Constants = {
+    // Beans company
+    beansApiKey: "QNOB-013X-0F32-LWJA",
+    beansStellarAccountId: "GBN767WEKXOG275FJQMEBK3XQXFOGQQ3FMI7GX7IJRSFMXYTZWQ7Y6Z6",
+  
+    // UnaSwap company
+    unaSwapApiKey: "QNOB-013X-0F32-LWJB",
+    unaSwapStellarAccountId: "GB5TLWF2UTZSY7QZ7BT6VK4UPT2KGG2TT33ZIK3JS74WVF7FKPWRQ7WN",
+  
+    // EkoEbooks company
+    ekoEbooksApiKey: "QNOB-013X-0F32-LWJC",
+    ekoEbooksStellarAccountId: "GBYSGYLHPDTMHX33DCITR6DA4CLDDEIPXBZ7ASNVYQPYPKA3ROQUSRHA",
+  
+    // USDC currency
+    usdStellarCurrencyId: 'f2155597-8629-40fd-ba65-7da8b5f837c3',
+    usdStellarCurrencyCode: 'USD',
+  
+    // XLM currency
+    xlmStellarCurrencyId: 'a14a2642-d650-4e6f-b72a-6ac86303a492',
+    xlmStellarCurrencyCode: 'XLM',
+  
+    // NGNC currency
+    ngnStellarCurrencyId: '98231a85-c20b-411f-bce9-746bfe92a368',
+    ngnStellarCurrencyCode: 'NGNC'
+  };
+  
+  export default Constants;
+  
